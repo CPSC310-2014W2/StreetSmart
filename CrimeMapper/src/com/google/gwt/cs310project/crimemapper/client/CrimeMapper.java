170936b254e490c0e821c97f863c112a510d5e2d
Haniel Martino <haniel_martino@hotmail.com>
2015-02-20 15:35:10 -0800

    Class definitions added

--- CrimeMapper/src/com/google/gwt/cs310project/crimemapper/client/CrimeMapper.java
+++ CrimeMapper/src/com/google/gwt/cs310project/crimemapper/client/CrimeMapper.java
@@ -1,6 +1,10 @@
 package com.google.gwt.cs310project.crimemapper.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TabPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 
 /**
@@ -8,9 +12,18 @@ import com.google.gwt.core.client.EntryPoint;
  */
 public class CrimeMapper implements EntryPoint {
 
+	// Create a tab panel with three tabs, each of which displays a different piece of text
+	private TabPanel mainPanel = new TabPanel();
+	private VerticalPanel tableVPanel = new VerticalPanel();
+	private FlexTable crimeFlexTable = new FlexTable();
+	private Label lastUpdatedLabel = new Label();
 	
 	public void onModuleLoad() {
-		// TODO Auto-generated method stub
+		// TODO Create tab to hold Map, Table and Settings
+		// TODO Assemble Table Panel
+		// TODO Assemble Main panel
+		// TODO Associate the Main panel with the HTML host page
+		
 		
 	}
 	
