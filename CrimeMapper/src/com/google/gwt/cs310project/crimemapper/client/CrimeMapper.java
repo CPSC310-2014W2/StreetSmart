6c869df19a3280e858b277bc19ceac42810ebcc8
Haniel Martino <haniel_martino@hotmail.com>
2015-02-20 21:31:08 -0800

    Tab Feature implemented

--- CrimeMapper/src/com/google/gwt/cs310project/crimemapper/client/CrimeMapper.java
+++ CrimeMapper/src/com/google/gwt/cs310project/crimemapper/client/CrimeMapper.java
@@ -18,7 +18,7 @@ import com.google.gwt.user.client.ui.TabLayoutPanel;
 public class CrimeMapper implements EntryPoint {
 
 	// Create a tab panel with three tabs, each of which displays a different piece of text
-	private TabLayoutPanel mainPanel = new TabLayoutPanel(4, Unit.EM);
+	private TabPanel mainPanel = new TabPanel();
 	private VerticalPanel tableVPanel = new VerticalPanel(); //holds flex table, reset panel
 	private FlexTable crimeFlexTable = new FlexTable();
 	private HorizontalPanel resetPanel = new HorizontalPanel();
@@ -31,7 +31,7 @@ public class CrimeMapper implements EntryPoint {
 	
 	public void onModuleLoad() {
 
-		// Create table for crime data.
+		// Create table and table headers for crime data.
 		crimeFlexTable.setText(0, 0, "Crime Type");
 		crimeFlexTable.setText(0, 1, "Mischief under $5000");
 		crimeFlexTable.setText(0, 2, "Mischief over $5000");
@@ -41,29 +41,42 @@ public class CrimeMapper implements EntryPoint {
 		crimeFlexTable.setText(0, 6, "Theft of auto over $5000");
 		crimeFlexTable.setText(0, 7, "Commercial break and enter");
 
-
 		// Assemble resetPanel.
 		resetPanel.add(clearTrendsButton);
-
-		// Create tab to hold Table, Map and Settings
-		mainPanel.add(new HTML("Table"), "[Table]");
-		mainPanel.add(new HTML("Map"), "[Map]");
-		mainPanel.add(new HTML("Settings"), "[Settings]");
-
-		// Assemble Table Panel
+		
+		// Assemble Table Panel to insert in Tab1 of Tab Panel
 		tableVPanel.add(crimeFlexTable);
 		tableVPanel.add(resetPanel);
 		tableVPanel.add(lastUploadedDateLabel);
+		
+		// Assemble temporary labels for Map and Settings tab
+		Label mapLabel = new Label("Map will go here");
+		Label settingsLabel = new Label("Settings will go here");
+		
+		// Label holders for settings and maps panel
+		mapLabel.setHeight("200");
+		settingsLabel.setHeight("200");
+		
+		//Create titles for tabs
+		  String tab1 = "Tabel";
+	      String tab2 = "Map";
+	      String tab3 = "Settings";
+	      
+		// Create tab to hold Table, Map and Settings
+	    // Assemble mainPanel
+		mainPanel.add(tableVPanel, tab1);
+		mainPanel.add(mapLabel, tab2);
+		mainPanel.add(settingsLabel, tab3);
 
-
-
-		// Assemble Main panel
-		// TODO Needs proper implementation
-
-		//mainPanel.add(tableVPanel, mainPanel.getTabWidget(0));
+		// first tab upon load
+		mainPanel.selectTab(0);
+		
+		// set width of mainPanel
+		mainPanel.setWidth("800");
+		mainPanel.setHeight("800");
 		
 		// Associate the Main panel with the HTML host page
-		RootPanel.get("crimeList").add(tableVPanel);
+		RootPanel.get("crimeList").add(mainPanel);
 	}
 }
 
