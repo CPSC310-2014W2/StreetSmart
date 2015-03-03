5c138ff3b34cee55a698eeb694c6b910dc8db1aa
Regina Adshade Moore <regina.adshade@gmail.com>
2015-03-02 19:37:42 -0800

    changed CrimeDataServiceImpl

--- CrimeMapper/src/com/google/gwt/cs310project/crimemapper/server/CrimeDataServiceImpl.java
+++ CrimeMapper/src/com/google/gwt/cs310project/crimemapper/server/CrimeDataServiceImpl.java
@@ -1,7 +1,14 @@
 package com.google.gwt.cs310project.crimemapper.server;
 
+import java.util.ArrayList;
+
+import com.google.gwt.cs310project.crimemapper.client.CrimeData;
+import com.google.gwt.cs310project.crimemapper.client.CrimeDataService;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
-public class CrimeDataServiceImpl extends RemoteServiceServlet {
+public class CrimeDataServiceImpl implements CrimeDataService {
 
+	public ArrayList<CrimeData> getCrimeData(String url) {
+		return null;
+	}
 }
