91fe833781d4aeb00a4972e80e278eab1c32a4be
Regina Adshade Moore <regina.adshade@gmail.com>
2015-03-02 19:18:23 -0800

    Added LoginServiceImpl Class

---
+++ CrimeMapper/src/com/google/gwt/cs310project/crimemapper/server/LoginServiceImpl.java
@@ -0,0 +1,8 @@
+package com.google.gwt.cs310project.crimemapper.server;
+
+public class LoginServiceImpl implements LoginService extends RemoteServiceServlet {
+
+	public LoginInfo login(String requestUri){
+		//TODO
+	}
+}
