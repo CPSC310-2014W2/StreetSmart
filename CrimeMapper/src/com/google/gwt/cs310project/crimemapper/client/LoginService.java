3c1497304ae760679aed09757ef56d7d113c20ae
Haniel Martino <haniel_martino@hotmail.com>
2015-03-02 19:15:12 -0800

    Added CrimeData, LoginService, LoginServiceAsync

---
+++ CrimeMapper/src/com/google/gwt/cs310project/crimemapper/client/LoginService.java
@@ -0,0 +1,10 @@
+package com.google.gwt.cs310project.crimemapper.client;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+@RemoteServiceRelativePath("login")
+public interface LoginService extends RemoteService {
+	public LoginInfo login(String requestUri);
+
+}
