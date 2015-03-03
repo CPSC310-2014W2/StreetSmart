af6a7f4b36f05f182089efa8e8cc5ca33390b1f3
Regina Adshade Moore <regina.adshade@gmail.com>
2015-03-02 19:22:51 -0800

    fixed errors in LoginServiceImpl

--- CrimeMapper/src/com/google/gwt/cs310project/crimemapper/server/LoginServiceImpl.java
+++ CrimeMapper/src/com/google/gwt/cs310project/crimemapper/server/LoginServiceImpl.java
@@ -1,6 +1,11 @@
 package com.google.gwt.cs310project.crimemapper.server;
 
-public class LoginServiceImpl implements LoginService extends RemoteServiceServlet {
+import org.eclipse.jetty.security.LoginService;
+
+import com.google.gwt.cs310project.crimemapper.client.LoginInfo;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+public class LoginServiceImpl extends RemoteServiceServlet implements LoginService {
 
 	public LoginInfo login(String requestUri){
 		//TODO
