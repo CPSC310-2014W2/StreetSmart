6cfc90228785c4e83aaa2ab31579c9106c4a2bab
jh1993 <johnnyhan1993@gmail.com>
2015-03-02 20:00:04 -0800

    Implemented half of getCrimeData(); didn't test yet

--- CrimeMapper/src/com/google/gwt/cs310project/crimemapper/server/CrimeDataServiceImpl.java
+++ CrimeMapper/src/com/google/gwt/cs310project/crimemapper/server/CrimeDataServiceImpl.java
@@ -1,6 +1,8 @@
 package com.google.gwt.cs310project.crimemapper.server;
 
 import java.util.ArrayList;
+import java.net.*;
+import java.io.*;
 
 import com.google.gwt.cs310project.crimemapper.client.CrimeData;
 import com.google.gwt.cs310project.crimemapper.client.CrimeDataService;
@@ -9,6 +11,26 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 public class CrimeDataServiceImpl implements CrimeDataService {
 
 	public ArrayList<CrimeData> getCrimeData(String url) {
+
+		ArrayList<CrimeData> crimeDataList = new ArrayList<CrimeData>();
+
+		try {
+			URL crime = new URL(url);
+			BufferedReader crimeIn = new BufferedReader(
+				new InputStreamReader(crime.openStream()));
+			String inputLine;
+			while ((inputLine = crimeIn.readLine()) != null) {
+				crimeDataList.add(parseCrimeDataLine(inputLine));
+			}
+		} catch (Exception e) {
+			// Assume the URL works correctly for now, so do nothing
+			e.printStackTrace();
+		}
+
+		return crimeDataList;
+	}
+	
+	private CrimeData parseCrimeDataLine(String inputLine) {
 		return null;
 	}
 }
