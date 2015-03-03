f762f54c0698d67edd1f50e0a1979270ef4dbdeb
Haniel Martino <haniel_martino@hotmail.com>
2015-03-02 23:26:27 -0800

    - Fixed errors in LoginServiceImpl - Added servlet mapping in web.xml

--- CrimeMapper/src/com/google/gwt/cs310project/crimemapper/client/LoginServiceAsync.java
+++ CrimeMapper/src/com/google/gwt/cs310project/crimemapper/client/LoginServiceAsync.java
@@ -1,6 +1,6 @@
 package com.google.gwt.cs310project.crimemapper.client;
 
-import com.google.gwt.cs310project.crimemapper.client.LoginInfo;
+//import com.google.gwt.cs310project.crimemapper.client.LoginInfo;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface LoginServiceAsync {
--- CrimeMapper/src/com/google/gwt/cs310project/crimemapper/server/LoginServiceImpl.java
+++ CrimeMapper/src/com/google/gwt/cs310project/crimemapper/server/LoginServiceImpl.java
@@ -1,32 +1,30 @@
 package com.google.gwt.cs310project.crimemapper.server;
 
-import org.eclipse.jetty.security.IdentityService;
-import org.eclipse.jetty.security.LoginService;
-import org.eclipse.jetty.server.UserIdentity;
-
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.gwt.cs310project.crimemapper.client.LoginInfo;
+import com.google.gwt.cs310project.crimemapper.client.LoginService;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
-@SuppressWarnings("serial")
-public class LoginServiceImpl extends RemoteServiceServlet implements LoginService {
+public class LoginServiceImpl extends RemoteServiceServlet implements
+    LoginService {
 
-	public LoginInfo login(String requestUri){
-		UserService userService = UserServiceFactory.getUserService();
-		User user = userService.getCurrentUser();
-		LoginInfo loginInfo = new LoginInfo();
-		
-		if(user != null){
-			loginInfo.setLoggedIn(true);
-			loginInfo.setEmailAddress(user.getEmail());
-		      loginInfo.setNickname(user.getNickname());
-		      loginInfo.setLogoutUrl(userService.createLogoutURL(requestUri));
-		    } else {
-		      loginInfo.setLoggedIn(false);
-		      loginInfo.setLoginUrl(userService.createLoginURL(requestUri));
-		    }
-		    return loginInfo;
-		  }
-	}
+  public LoginInfo login(String requestUri) {
+    UserService userService = UserServiceFactory.getUserService();
+    User user = userService.getCurrentUser();
+    LoginInfo loginInfo = new LoginInfo();
+
+    if (user != null) {
+      loginInfo.setLoggedIn(true);
+      loginInfo.setEmailAddress(user.getEmail());
+      loginInfo.setNickname(user.getNickname());
+      loginInfo.setLogoutUrl(userService.createLogoutURL(requestUri));
+    } else {
+      loginInfo.setLoggedIn(false);
+      loginInfo.setLoginUrl(userService.createLoginURL(requestUri));
+    }
+    return loginInfo;
+  }
+
+}
