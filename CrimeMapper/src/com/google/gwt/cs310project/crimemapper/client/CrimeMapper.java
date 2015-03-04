5c64317addb30118756ed1a5b1ae4fa14a6993d5
Haniel Martino <haniel_martino@hotmail.com>
2015-03-03 16:59:05 -0800

    - Big UI update - Added a FAQ tab - Refactored fields in CrimeMapper class - Refactored handlers into their own method - Added a prompt in text box to load crime data - Removed some unused imports

--- CrimeMapper/src/com/google/gwt/cs310project/crimemapper/client/CrimeMapper.java
+++ CrimeMapper/src/com/google/gwt/cs310project/crimemapper/client/CrimeMapper.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
@@ -15,7 +14,7 @@ import com.google.gwt.user.client.ui.HTMLTable.Cell;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Panel;
-import com.google.gwt.user.client.ui.TabLayoutPanel;
+import com.google.gwt.user.client.ui.StackPanel;
 import com.google.gwt.user.client.ui.TabPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -28,81 +27,115 @@ import com.google.gwt.user.client.ui.RootPanel;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class CrimeMapper implements EntryPoint {
-	
-	// Create a tab panel with three tabs, each of which displays a different piece of text
-	private VerticalPanel mainPanel = new VerticalPanel();
+
+	// Dynamic Panels
 	private TabPanel tabPanel = new TabPanel();
+	private StackPanel faqPanel = new StackPanel();
+	
+	// Static Panels
+	private VerticalPanel mainPanel = new VerticalPanel();
 	private VerticalPanel tableVPanel = new VerticalPanel(); // holds flex table, reset panel
 	private VerticalPanel settingsVPanel = new VerticalPanel();
 	private VerticalPanel mapsVPanel = new VerticalPanel();
-	private FlexTable crimeFlexTable = new FlexTable();
 	private HorizontalPanel clearTrendsButtonPanel = new HorizontalPanel();
-	private Label lastUploadedDateLabel = new Label();
+	
+	// Table features
+	private FlexTable crimeFlexTable = new FlexTable();
 	private Button clearTrendsButton = new Button("Clear Trends");
-	private Button loadCrimeDataButton = new Button("Load Data");
-	private TextBox newUrlTextBox = new TextBox();
-	private final int NO_SELECTION_FLAG = -1;
+	private Label lastUploadedDateLabel = new Label();
 	private int selectedRow;
 	
+	private Button loadCrimeDataButton = new Button("Load Data");
+	
+	// Settings Text Box flags
+	private TextBox newUrlTextBox = new TextBox();
+	private final int NO_TABLE_SELECTION_FLAG = -1;
+	private final int CLEAR_TEXT_BOX_FLAG = -1;
+	private int selectedTextBox = CLEAR_TEXT_BOX_FLAG;
+	
+	//Crime Types
+	private String crime1 = "Mischief under $5000";
+	private String crime2 = "Mischief over $5000";
+	private String crime3 = "Theft from auto under $5000";
+	private String crime4 = "Theft from auto over $5000";
+	private String crime5 = "Theft of auto under $5000";
+	private String crime6 = "Theft of auto over $5000";
+	private String crime7 = "Commercial break and enter";
+
 	//CrimeData RPC fields
 	private ArrayList<CrimeData> crimes = new ArrayList<CrimeData>();
 	private CrimeDataServiceAsync crimeDataSvc = GWT.create(CrimeDataService.class);
-	
+
 	/**
 	 * Entry point method.
 	 */
-	
+
 	public void onModuleLoad() {
 		
-		// Listen for mouse events on the Add button.
-	    loadCrimeDataButton.addClickHandler(new ClickHandler() {
-	      public void onClick(ClickEvent event) {
-	    	  String crimeURL = newUrlTextBox.getText();
-	    	  refreshCrimeList(crimeURL);
-	    	  newUrlTextBox.setText("");
-	      }
-	    });
-	    
-	    // Listen for mouse click on the Rows in table and Highlight row.
-	    crimeFlexTable.addClickHandler(new ClickHandler(){
-	    	public void onClick(ClickEvent event){
-	    		Cell cell = crimeFlexTable.getCellForEvent(event);
-	    		int receiverRowIndex = cell.getRowIndex();
-	    		if (receiverRowIndex == selectedRow)
-	    		{
-	    			crimeFlexTable.getRowFormatter().setStyleName(receiverRowIndex, "rowUnselectedShadow");
-	    			selectedRow = NO_SELECTION_FLAG;
-	    		} else {
-	    			crimeFlexTable.getRowFormatter().setStyleName(receiverRowIndex, "rowSelectedShadow");
-	    			selectedRow = receiverRowIndex;
-	    		}
-	    	}
-	    });
-	    
+		applicationHandlers();
+		
 		// Associate the Main panel with the HTML host page
 		RootPanel.get("crimeList").add(buildMainPanel());
 	}
-	
+
+	private void applicationHandlers(){
+		// Clear Text box when mouse places icon
+		newUrlTextBox.addClickHandler(new ClickHandler(){
+			public void onClick(ClickEvent event){
+				if(selectedTextBox == CLEAR_TEXT_BOX_FLAG){
+					newUrlTextBox.setText("");
+					selectedTextBox = 0;
+					} 
+			}
+		});
+
+		// Listen for mouse events on the Add button.
+		loadCrimeDataButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				String crimeURL = newUrlTextBox.getText();
+				refreshCrimeList(crimeURL);
+				newUrlTextBox.setText("Paste Crime URL here");
+				selectedTextBox = CLEAR_TEXT_BOX_FLAG;
+			}
+		});
+
+		// Listen for mouse click on the Rows in table and Highlight row.
+		crimeFlexTable.addClickHandler(new ClickHandler(){
+			public void onClick(ClickEvent event){
+				Cell cell = crimeFlexTable.getCellForEvent(event);
+				int receiverRowIndex = cell.getRowIndex();
+				if (receiverRowIndex == selectedRow)
+				{
+					crimeFlexTable.getRowFormatter().setStyleName(receiverRowIndex, "rowUnselectedShadow");
+					selectedRow = NO_TABLE_SELECTION_FLAG;
+				} else {
+					crimeFlexTable.getRowFormatter().setStyleName(receiverRowIndex, "rowSelectedShadow");
+					selectedRow = receiverRowIndex;
+				}
+			}
+		});
+	}
 	/**
 	 * Method for constructing Main Panel
 	 */
 	private Panel buildMainPanel(){
 		mainPanel.add(buildTabPanel());
-		
+
 		return mainPanel;
 	}
-	
+
 	/**
 	 * Method for constructing Tab Panel
 	 */
 	private TabPanel buildTabPanel(){
-		
+
 		tabPanel.setAnimationEnabled(true);
-		
+
 		//Create titles for tabs
 		String tab1Title = "Trends";
 		String tab2Title = "Map";
 		String tab3Title = "Settings";
+		String tab4Title = "FAQ";
 
 		//Create Custom FlowPanels to add to TabPanel
 		FlowPanel flowpanel;
@@ -111,51 +144,55 @@ public class CrimeMapper implements EntryPoint {
 		flowpanel = new FlowPanel();
 		flowpanel.add(buildTableTabPanel());
 		tabPanel.add(flowpanel, tab1Title);
-		
+
 		flowpanel = new FlowPanel();
 		flowpanel.add(buildMapTabPanel());
 		tabPanel.add(flowpanel, tab2Title);
-		
+
 		flowpanel = new FlowPanel();
 		flowpanel.add(buildSettingsTabPanel());
 		tabPanel.add(flowpanel, tab3Title);
-		
+
+		flowpanel = new FlowPanel();
+		flowpanel.add(buildFaqTabPanel());
+		tabPanel.add(flowpanel, tab4Title);
+
 		// first tab upon load
 		tabPanel.selectTab(0);
 		tabPanel.setSize("1200px", "1200px");
-		
+
 
 		return tabPanel;
 	}
-	
+
 	/**
 	 * Method for constructing Table Tab Panel 
 	 *  - style elements for table
 	 */
 	private Panel buildTableTabPanel(){
-		
+
 		// Create table and table headers for crime data.
 		crimeFlexTable.setText(1, 0, "Year");
 		crimeFlexTable.setText(0, 1, "Crime Type");
-		crimeFlexTable.setText(1, 1, "Mischief under $5000");
-		crimeFlexTable.setText(1, 2, "Mischief over $5000");
-		crimeFlexTable.setText(1, 3, "Theft from auto under $5000");
-		crimeFlexTable.setText(1, 4, "Theft from auto over $5000");
-		crimeFlexTable.setText(1, 5, "Theft of auto under $5000");
-		crimeFlexTable.setText(1, 6, "Theft of auto over $5000");
-		crimeFlexTable.setText(1, 7, "Commercial break and enter");
-		
+		crimeFlexTable.setText(1, 1, crime1);
+		crimeFlexTable.setText(1, 2, crime2);
+		crimeFlexTable.setText(1, 3, crime3);
+		crimeFlexTable.setText(1, 4, crime4);
+		crimeFlexTable.setText(1, 5, crime5);
+		crimeFlexTable.setText(1, 6, crime6);
+		crimeFlexTable.setText(1, 7, crime7);
+
 		int row = crimeFlexTable.getRowCount();
 		// TEST ROW 
 		crimeFlexTable.setText(row, 0, "2003");
 		// TODO Possibly use enum to denote crime types and
 		// a loop to automatically add all crime types to table
-		
+
 		// Merging Crime Type to be over the Crime Types
 		FlexCellFormatter crimeTypeCellFormatter = crimeFlexTable.getFlexCellFormatter();
 		crimeTypeCellFormatter.setHorizontalAlignment(0, 1, HasHorizontalAlignment.ALIGN_CENTER);
 		crimeTypeCellFormatter.setColSpan(0, 1, 7);
-		
+
 		// Add styles to elements in the crime type table
 		crimeFlexTable.addStyleName("crimeData");
 		crimeFlexTable.getCellFormatter().addStyleName(0, 0, "crimeTypeHeader");
@@ -170,27 +207,26 @@ public class CrimeMapper implements EntryPoint {
 		}
 		crimeFlexTable.setCellPadding(20);
 
-		
+
 		// Assemble resetPanel.
 		clearTrendsButtonPanel.add(clearTrendsButton);
 
 		// Date label
 		lastUploadedDateLabel.setText("MOST RECENT UPDATE DATE GOES HERE");
-		
-		
+
+
 		tableVPanel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
 
 		// Assemble Table Panel to insert in Tab1 of Tab Panel
 		tableVPanel.add(crimeFlexTable);
 		tableVPanel.add(clearTrendsButtonPanel);
 		tableVPanel.add(lastUploadedDateLabel);
-		
-		
+
+
 		// return table constructed panel
 		return tableVPanel;
-
 	}
-	
+
 	/**
 	 *  Method for Constructing Map tab panel
 	 */
@@ -202,50 +238,103 @@ public class CrimeMapper implements EntryPoint {
 		mapsVPanel.add(mapLabel);
 		return mapsVPanel;
 	}
-	
+
 	/**
 	 * Method for Constructing Settings tab panel
 	 */
 	private Panel buildSettingsTabPanel(){
 		// Assemble elements for Settings Panel
 		Label settingsLabel = new Label("SETTINGS WILL GO HERE");
-		
+
 		// Assemble Settings Panel to insert Settings 
 		settingsVPanel.add(settingsLabel);
-		newUrlTextBox.setName("Paste Crime Data URL");
+		newUrlTextBox.setText("Paste Crime URL here");
 		settingsVPanel.add(newUrlTextBox);
 		settingsVPanel.add(loadCrimeDataButton);
-		
+
 		return settingsVPanel;
 	}
-	
+
+	private Panel buildFaqTabPanel(){
+		// Application facts
+		String appFact1 = "Fact 1";
+		String appFact2 = "Fact 2";
+		String appFact3 = "Fact 3";
+
+		// Crime Types
+		String cr1 = "Explanation 1";
+		String cr2 = "Explanation 2";
+		String cr3 = "Explanation 3";
+		String cr4 = "Explanation 4";
+		String cr5 = "Explanation 5";
+		String cr6 = "Explanation 6";
+		String cr7 = "Explanation 7";
+
+		Label label;
+
+		//Application Facts
+		label = new Label(appFact1);
+		faqPanel.add(label, "App Fact1", false);
+
+		label = new Label(appFact2);
+		faqPanel.add(label, "App Fact2", false);
+
+		label = new Label(appFact3);
+		faqPanel.add(label, "App Fact3", false);
+
+		//Crime facts
+		label = new Label(cr1);
+		faqPanel.add(label, crime1, false);
+
+		label = new Label(cr2);
+		faqPanel.add(label, crime2, false);
+
+		label = new Label(cr3);
+		faqPanel.add(label, crime3, false);
+
+		label = new Label(cr4);
+		faqPanel.add(label, crime4, false);
+
+		label = new Label(cr5);
+		faqPanel.add(label, crime5, false);
+
+		label = new Label(cr6);
+		faqPanel.add(label, crime6, false);
+
+		label = new Label(cr7);
+		faqPanel.add(label, crime7, false);
+
+		faqPanel.setSize("100%","100%");
+		return faqPanel;
+	}
+
 	/**
 	 * Add crimedata to FlexTable 
 	 * Added when admin clicks add new data
 	 * 
 	 */
-	
+
 	private void refreshCrimeList(String crimeURL){
 		//Initialize the service proxy.
 		if(crimeDataSvc == null){
 			crimeDataSvc = GWT.create(CrimeDataService.class);
 		}
-		
+
 		// Set up the callback object.
 		AsyncCallback<ArrayList<CrimeData>> callback = new AsyncCallback<ArrayList<CrimeData>>(){
 			public void onFailure(Throwable caught){
 				//TODO: Do something with errors.
 			}
-			
+
 			public void onSuccess(ArrayList<CrimeData> result){
 				updateTable(result);
 			}
 		};
-		
+
 		// Make the call to the stock price service.
 		crimeDataSvc.getCrimeData(crimeURL, callback);
 	}
-	
+
 	private void updateTable(ArrayList<CrimeData> crimes){}
 }
 
