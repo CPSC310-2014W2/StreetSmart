778545609851843d844cd1ab21fd845f3555a930
Regina Adshade Moore <regina.adshade@gmail.com>
2015-03-07 14:45:08 -0800

    Removed double spacing on getTrends method

--- CrimeMapper/src/com/google/gwt/cs310project/crimemapper/client/CrimeMapper.java
+++ CrimeMapper/src/com/google/gwt/cs310project/crimemapper/client/CrimeMapper.java
@@ -471,41 +471,23 @@ public class CrimeMapper implements EntryPoint {
 	}
 	
 	private ArrayList<ArrayList<Integer>> getTrends(int index) {
-
 		ArrayList<ArrayList<Integer>> trendsByYear = new ArrayList<>();
-
 		if (index<START_OF_DATA_ROWS) {return null;}
-
 		int baseYear = getYearFromTable(index);
-
 		CrimeDataByYear baseYearCrimeData = crimeDataMap.get(baseYear);
-
 		for (Map.Entry<Integer, CrimeDataByYear> otherYear: crimeDataMap.entrySet()){
-
 			CrimeDataByYear otherYearCrimeData = otherYear.getValue();
-
 			ArrayList<Integer> trendsByType = new ArrayList<>();
-
 			for (int i = 0; i < CrimeTypes.getNumberOfTypes(); i++) {
-
 				String type = CrimeTypes.getType(i);
-
 				int base = baseYearCrimeData.getNumberOfCrimeTypeOccurrences(type);
-
 				int other = otherYearCrimeData.getNumberOfCrimeTypeOccurrences(type);
-
 				int percentChange = (((other-base)/base)*100);
-
 				trendsByType.add(percentChange);
-
 			}
-
 			trendsByYear.add(trendsByType);
-
 		}
-
 		return trendsByYear;
-
 }
 	
 	/**
@@ -515,7 +497,6 @@ public class CrimeMapper implements EntryPoint {
 	private void updateTableTrends(ArrayList<ArrayList<Integer>> trendsByYear) {
 
 	}
-
 
 }
 
