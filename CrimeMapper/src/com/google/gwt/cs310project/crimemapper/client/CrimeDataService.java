3200b5dfb93c31d9b1afe55758726c3f57e3e630
Haniel Martino <haniel_martino@hotmail.com>
2015-03-10 17:32:42 -0700

    - Johnny's changes are in this file

--- CrimeMapper/src/com/google/gwt/cs310project/crimemapper/client/CrimeDataService.java
+++ CrimeMapper/src/com/google/gwt/cs310project/crimemapper/client/CrimeDataService.java
@@ -3,6 +3,7 @@ package com.google.gwt.cs310project.crimemapper.client;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
+import java.util.ArrayList;
 import java.util.TreeMap;
 
 @RemoteServiceRelativePath("crimeData")
@@ -10,4 +11,6 @@ public interface CrimeDataService extends RemoteService {
 	CrimeDataByYear getCrimeDataByYear(String url);
 	TreeMap<Integer, CrimeDataByYear> getCrimeDataMap();
 	void setCrimeDataMap(TreeMap<Integer, CrimeDataByYear> crimeDataMap);
+	ArrayList<String> getAdminAccounts();
+	void setAdminAccounts(ArrayList<String> admins);
 }
--- CrimeMapper/src/com/google/gwt/cs310project/crimemapper/server/CrimeDataServiceImpl.java
+++ CrimeMapper/src/com/google/gwt/cs310project/crimemapper/server/CrimeDataServiceImpl.java
@@ -1,13 +1,13 @@
 package com.google.gwt.cs310project.crimemapper.server;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 import java.util.TreeMap;
+import java.net.*;
+import java.io.*;
+import java.util.List;
 
+import javax.jdo.JDOException;
 import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManager;
 import javax.jdo.PersistenceManagerFactory;
@@ -22,11 +22,11 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 public class CrimeDataServiceImpl extends RemoteServiceServlet implements CrimeDataService {
 
 	// Data persistence fields
-	//private static final Logger LOG = Logger.getLogger(CrimeMapper.class.getName());
 	private static final PersistenceManagerFactory PMF =
 			JDOHelper.getPersistenceManagerFactory("transactions-optional");
 
 	public CrimeDataByYear getCrimeDataByYear(String url) {
+		// TODO: Shouldn't we make this method static?
 
 		ArrayList<CrimeData> crimeDataList = new ArrayList<CrimeData>();
 		crimeDataList = parseCrimeData(url);
@@ -85,23 +85,12 @@ public class CrimeDataServiceImpl extends RemoteServiceServlet implements CrimeD
 		return new CrimeData(type, year, month, location);
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public TreeMap<Integer, CrimeDataByYear> getCrimeDataMap() {
 		TreeMap<Integer, CrimeDataByYear> crimeDataMap = null;
 		PersistenceManager pm = getPersistenceManager();
 		try {
-			/*
-			 * TODO: If crimeDataMap isn't the only TreeMap we have
-			 * among all our persistent data, we might need to
-			 * change this.
-			 */
-			Query q = pm.newQuery(GlobalPersistentData.class);
-			List<GlobalPersistentData> globalPersistentDataList =
-					(List<GlobalPersistentData>) q.execute();
-			int size = globalPersistentDataList.size();
-			assert size == 1 || size == 0;
-			GlobalPersistentData globalPersistentData = getGlobalPersistentData();
+			GlobalPersistentData globalPersistentData = getGlobalPersistentData(pm);
 			crimeDataMap = globalPersistentData.getCrimeDataMap();
 			if (crimeDataMap == null) {
 				crimeDataMap = new TreeMap<Integer, CrimeDataByYear>();
@@ -115,34 +104,54 @@ public class CrimeDataServiceImpl extends RemoteServiceServlet implements CrimeD
 	
 	@Override
 	public void setCrimeDataMap(TreeMap<Integer, CrimeDataByYear> crimeDataMap) {
-		GlobalPersistentData globalPersistentData = getGlobalPersistentData();
-		globalPersistentData.setCrimeDataMap(crimeDataMap);
-	}
-
-	@SuppressWarnings("unchecked")
-	private GlobalPersistentData getGlobalPersistentData() {
-		GlobalPersistentData globalPersistentData = null;
 		PersistenceManager pm = getPersistenceManager();
 		try {
-			/*
-			 * TODO: If crimeDataMap isn't the only TreeMap we have
-			 * among all our persistent data, we might need to
-			 * change this.
-			 */
-			Query q = pm.newQuery(GlobalPersistentData.class);
-			List<GlobalPersistentData> globalPersistentDataList =
-					(List<GlobalPersistentData>) q.execute();
-			int size = globalPersistentDataList.size();
-			assert size == 1 || size == 0;
-			if (size == 0) {
-				globalPersistentData = new GlobalPersistentData();
-				globalPersistentData.setCrimeDataMap(new TreeMap<Integer, CrimeDataByYear>());
-				pm.makePersistent(globalPersistentData);
-			} else {
-				globalPersistentData = globalPersistentDataList.get(0);
+			getGlobalPersistentData(pm).setCrimeDataMap(crimeDataMap);
+		} finally {
+			pm.close();
+		}
+	}
+	
+	@Override
+	public ArrayList<String> getAdminAccounts() {
+		ArrayList<String> adminAcct = null;
+		PersistenceManager pm = getPersistenceManager();
+		try {
+			GlobalPersistentData globalPersistentData = getGlobalPersistentData(pm);
+			adminAcct = globalPersistentData.getAdminAccounts();
+			if (adminAcct == null) {
+				adminAcct = new ArrayList<String>();
+				globalPersistentData.setAdminAccounts(adminAcct);
 			}
 		} finally {
 			pm.close();
+		}
+		return new ArrayList<String>();
+	}
+	
+	@Override
+	public void setAdminAccounts(ArrayList<String> admins) {
+		PersistenceManager pm = getPersistenceManager();
+		try {
+			getGlobalPersistentData(pm).setAdminAccounts(admins);
+		} finally {
+			pm.close();
+		}
+	}
+	
+	private GlobalPersistentData getGlobalPersistentData(PersistenceManager pm) {
+		GlobalPersistentData globalPersistentData = null;
+		Query q = pm.newQuery(GlobalPersistentData.class);
+		List<GlobalPersistentData> globalPersistentDataList =
+				(List<GlobalPersistentData>) q.execute();
+		int size = globalPersistentDataList.size();
+		assert size == 1 || size == 0;
+		if (size == 0) {
+			globalPersistentData = new GlobalPersistentData();
+			globalPersistentData.setCrimeDataMap(new TreeMap<Integer, CrimeDataByYear>());
+			pm.makePersistent(globalPersistentData);
+		} else {
+			globalPersistentData = globalPersistentDataList.get(0);
 		}
 		return globalPersistentData;
 	}
